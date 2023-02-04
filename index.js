c37783ff374efcaf2648166a6ffa2261f081524b
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2023-02-04 12:43:34 +0530

    Taking input

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
 let passwordEl1 = document.getElementById("password-el-1")
 let passwordEl2 = document.getElementById("password-el-2")
+let inputEl = document.getElementById("input")
 
 
 
-let passwordLength = 12
+let passwordLength = 12;
 
 
 
-function randomChar(){
+function randomChar() {
     let randomChar = Math.floor(Math.random() * characters.length)
     return characters[randomChar]
 }
 
-function generatePassword(){
+function generatePassword() {
     let password = ""
-    for(let i = 0; i<passwordLength; i++){
-        password+= randomChar()
+    for (let i = 0; i < passwordLength; i++) {
+        password += randomChar()
     }
     return password
 }
 
 
-function renderPasswords(){
-    
+function renderPasswords() {
+
     const passwordOne = generatePassword()
     const passwordTwo = generatePassword()
     passwordEl1.textContent = passwordOne

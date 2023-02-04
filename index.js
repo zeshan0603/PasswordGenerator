dd927546ab1f34c55ed42b6386cfee7cc54b196e
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2023-02-04 11:54:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordEl1 = document.getElementById("password-el-1")
+let passwordEl2 = document.getElementById("password-el-2")
+
+
+
+let passwordLength = 12
+
+
+
+function randomChar(){
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generatePassword(){
+    let password = ""
+    for(let i = 0; i<passwordLength; i++){
+        password+= randomChar()
+    }
+    return password
+}
+
+
+function renderPasswords(){
+    
+    const passwordOne = generatePassword()
+    const passwordTwo = generatePassword()
+    passwordEl1.textContent = passwordOne
+    passwordEl2.textContent = passwordTwo
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
